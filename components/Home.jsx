014e33ddf825f5493173f8eebc91f26b0f94ac04
VictorHMSforne <vmendessforne@gmail.com>
2026-06-14 14:15:58 -0300

    feat: Iniciando projeto geolize

---
+++ components/Home.jsx
@@ -0,0 +1,21 @@
+function Home(){
+    return (
+        <main>
+            <HeroSection />
+            
+        </main>
+    );
+}
+
+function HeroSection(){
+    return (
+        <section className="hero">
+            <div className="hero-content">
+                <h1>GEOLIZE</h1>
+                <p>
+                    O GEOLIZE É FODA E VAMO FICAR RICO 
+                </p>
+            </div>
+        </section>
+    );
+}
