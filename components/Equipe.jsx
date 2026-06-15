cd38b44834cc420ffa1a145ec1ca378030f7ed9d
VictorHMSforne <vmendessforne@gmail.com>
2026-06-15 09:28:14 -0300

    feat: adicionado imagens dos colabs

---
+++ components/Equipe.jsx
@@ -0,0 +1,41 @@
+function Equipe() {
+    return (
+        <section id="equipe">
+            <h2>Equipe</h2>
+
+            <div className="equipe-container">
+
+                <div className="membro-card">
+                    <img src="img/hector.jpg" alt="Hector de Santana Lima" />
+                    <h3>Hector de Santana Lima</h3>
+                    <p>Desenvolvedor Front-End</p>
+                </div>
+
+                <div className="membro-card">
+                    <img src="img/matheus.jpg" alt="Matheus Ferreira da Rocha" />
+                    <h3>Matheus Ferreira da Rocha</h3>
+                    <p>Suporte</p>
+                </div>
+
+                <div className="membro-card">
+                    <img src="img/thiago.jpg" alt="Thiago Campanholi Chagas" />
+                    <h3>Thiago Campanholi Chagas</h3>
+                    <p>Banco de Dados</p>
+                </div>
+
+                <div className="membro-card">
+                    <img src="img/victor.jpg" alt="Victor Hugo Mendes Sforne" />
+                    <h3>Victor Hugo Mendes Sforne</h3>
+                    <p>Back-End</p>
+                </div>
+
+                <div className="membro-card">
+                    <img src="img/wellington.jpg" alt="Wellington Fernando Rocha" />
+                    <h3>Wellington Fernando Rocha</h3>
+                    <p>Documentação</p>
+                </div>
+
+            </div>
+        </section>
+    );
+}
